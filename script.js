816ae412929dc6cd5ca71bcd3a214347e883ae34
Jaycee-Inocencio <oicneconinhoj@gmail.com>
2026-01-09 13:38:26 +0800

    added saving contact as vcf file

--- script.js
+++ script.js
@@ -3,10 +3,39 @@ function saveContact(){
     saveButton.innerText = "Saving...";
     saveButton.disabled = true;
     saveButton.style.backgroundColor = "#96b9e9";
+    saveFile();
     setTimeout(() => {
         saveButton.innerText = "Saved!";
         saveButton.style.backgroundColor = "#3a7bd5";
     }, 2000);
+}
+function saveFile(){
+      const vCardData = `
+BEGIN:VCARD
+VERSION:3.0
+N:Villafuerte;Erick;Yosorez;;;
+FN:Erick Yosorez Villafuerte
+TITLE:HR Specialist
+ORG:Addleman
+TEL;TYPE=CELL:09633621187
+TEL;TYPE=VIBER:09633621187
+TEL;TYPE=WHATSAPP:09633621187
+EMAIL:eyr.addleman@gmail.com
+END:VCARD
+  `.trim();
+
+  const blob = new Blob([vCardData], { type: "text/vcard;charset=utf-8" });
+  const url = URL.createObjectURL(blob);
+
+  const link = document.createElement("a");
+  link.href = url;
+  link.download = "Erick_Yosorez_Villafuerte.vcf";
+
+  document.body.appendChild(link);
+  link.click();
+
+  document.body.removeChild(link);
+  URL.revokeObjectURL(url);
 }
     // alert("Contact saved!");
 // }
